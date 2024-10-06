5c1e2d5912542fc3c6c6b09f29bf6b85225ebbc6
IvanKaigorodov <kil-42@yandex.ru>
2024-10-07 04:04:00 +0700

    add first homework

---
+++ 06_lesson/lesson_6_task_2.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("http://uitestingplayground.com/textinput")
+
+pole_name = driver.find_element(By.CSS_SELECTOR, "#newButtonName")
+pole_name.send_keys("SkyPro")
+
+button = driver.find_element(By.CSS_SELECTOR, "#updatingButton").click()
+
+txt_button = driver.find_element(By.CSS_SELECTOR, "#updatingButton").text
+
+print(txt_button)
+
+driver.quit()
