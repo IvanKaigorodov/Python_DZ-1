8abbc2bef31f73b95f021329f7cc1cfb99e73382
IvanKaigorodov <kil-42@yandex.ru>
2024-11-02 19:51:35 +0700

    add first homework

--- 09_lesson/SubjectTable.py
+++ 09_lesson/SubjectTable.py
@@ -4,16 +4,42 @@ from sqlalchemy.sql import text
 
 class SadjtctTable:
     scripts = {
+                
+        "select by": text("select * from subject\
+            where subject_title = :db_title"),
+
+        "select by up": text("select * from subject\
+            where subject_title = :db_up_title"),
+
+        "select by id": text("select * from subject\
+            where subject_id = :db_id"),
+
         "insert new": text("insert into subject\
             (subject_title, subject_id) values (:new_title, :id)"),
+        
         "delete by id": text("delete from subject\
             where subject_id = :id_to_delete"),
+        
         "update title": text("update subject\
-            set subject_title = :up_title where subject_id = :up_id")
+            set subject_title = :up_title where subject_id = :up_id"),
+
     }
 
+        
     def __init__(self, connection_string):
         self.db = create_engine(connection_string)
+
+    def get_subject(self, new_title):
+        return self.db.execute(self.scripts["select by"],
+        db_title = new_title).fetchall()
+    
+    def get_subject_up(self, up_title):
+        return self.db.execute(self.scripts["select by up"],
+        db_up_title = up_title).fetchall()
+    
+    def get_subject_del(self, id):
+        self.db.execute(self.scripts["select by id"],
+        db_id = id).fetchall()
 
     def create_title(self, new_title, id):
         self.db.execute(self.scripts["insert new"],
--- 09_lesson/test_subject_db.py
+++ 09_lesson/test_subject_db.py
@@ -6,15 +6,41 @@ db = SadjtctTable("postgresql://postgres:"
             "SkyproIvan142@localhost:5432/postgres")
 
 
-def test_edit_subject_title():
-    # Новый предмет
-    new_title = 'Python-db'
-    id = '100'
+def test_add_subject():
+    # Добавить в базу новый предмет
+    new_title = 'Новый предмет'
+    id = 100
     db.create_title(new_title, id)
+    # Выбрать из базы новый предмет
+    add_subject = db.get_subject(new_title)
+    db.delete_id(id)
+    # Проверить совпадение названия предметов
+    assert [(id, new_title)] == add_subject
 
+
+def test_edit_subject_title():
+    # Добавить в базу новый предмет
+    new_title = 'Новый предмет'
+    id = 100
+    db.create_title(new_title, id)
     #  Изменение названия предмета
-    up_title = 'NEW'
+    up_title = 'Заменил название'
     db.update_title(up_title, id)
-
+    edit_subject = db.get_subject_up(up_title)
     # Удаление предмета
     db.delete_id(id)
+    # Проверить совпадения измененного названия
+    assert [(id, up_title)] == edit_subject
+
+
+def test_del_subject():
+    # Добавить в базу новый предмет
+    new_title = 'Новый предмет'
+    id = 100
+    db.create_title(new_title, id)
+    # Выбрать из базы новый предмет по id
+    rows = db.get_subject_del(id)
+    # Удалить из базы новый предмет по id
+    db.delete_id(id)
+    # Проверить отсутствие предмета по id
+    assert rows == None
